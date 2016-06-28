271b380333bffe51f9a568149abb992c41f21f96
apdaza <apdaza@gmail.com>
2016-06-27 21:19:47 -0500

    first commit

--- controllers/main.js
+++ controllers/main.js
@@ -5,50 +5,91 @@ var rulesApp = angular.module('rulesApp', ['ngRoute']);
 rulesApp.config(function($routeProvider) {
 
     $routeProvider
-        .when('/main', {
-            templateUrl : 'views/main.html',
-            controller  : 'mainController'
-        })
-        .when('/about', {
-            templateUrl : 'views/about.html',
-            controller  : 'aboutController'
-        })
         .when('/home', {
             templateUrl : 'views/home.html',
             controller  : 'homeController'
         })
-        .when('/users', {
-            templateUrl : 'views/users.html',
-            controller  : 'userController'
+        .when('/listarDominios', {
+            templateUrl : 'views/listarDominios.html',
+            controller  : 'domainController'
+        })
+        .when('/agregarDominio', {
+            templateUrl : 'views/agregarDominio.html',
+            controller  : 'addDomainController'
+        })
+        .when('/listarReglas', {
+            templateUrl : 'views/listarReglas.html',
+            controller  : 'ruleController'
+        })
+        .when('/agregarRegla', {
+            templateUrl : 'views/agregarRegla.html',
+            controller  : 'addRuleController'
+        })
+        .when('/listarComponentes', {
+            templateUrl : 'views/listarComponentes.html',
+            controller  : 'componentController'
+        })
+        .when('/agregarComponente', {
+            templateUrl : 'views/agregarComponente.html',
+            controller  : 'addComponentController'
+        })
+        .when('/about', {
+            templateUrl : 'views/about.html',
+            controller  : 'aboutController'
         })
         .otherwise({
             redirectTo: '/home'
         });
 });
 
-rulesApp.controller('mainController', function($scope) {
-    $scope.message = 'Hola, Mundo!';
+rulesApp.controller('homeController', function($scope) {
+  $scope.message = 'Versión 0.0.1';
+});
+
+rulesApp.controller('domainController', function($scope, $http) {
+  $scope.title = 'Dominios';
+  $scope.message = 'Listado de Dominios de Aplicación';
+
+  $http.get('http://localhost:8080/api/rules/domains')
+  .then(function(response) {
+      $scope.data = response.data;
+  });
+});
+rulesApp.controller('addDomainController', function($scope) {
+  $scope.title = 'Dominios';
+  $scope.message = 'Agregar Dominio de Aplicación';
+});
+
+rulesApp.controller('ruleController', function($scope, $http) {
+  $scope.title = 'Reglas';
+  $scope.message = 'Listado de Reglas de Aplicación';
+
+  $http.get('http://localhost:8080/api/rules/rules')
+  .then(function(response) {
+      $scope.data = response.data;
+  });
+});
+rulesApp.controller('addRuleController', function($scope) {
+  $scope.title = 'Reglas';
+  $scope.message = 'Agregar Regla de Aplicación';
+});
+
+rulesApp.controller('componentController', function($scope, $http) {
+  $scope.title = 'Componentes';
+  $scope.message = 'Listado de Componentes de Reglas de Aplicación';
+
+  $http.get('http://localhost:8080/api/rules/components')
+  .then(function(response) {
+      $scope.data = response.data;
+  });
+});
+rulesApp.controller('addComponentController', function($scope) {
+  $scope.title = 'Componentes';
+  $scope.message = 'Agregar Componentes de Reglas de Aplicación';
 });
 
 rulesApp.controller('aboutController', function($scope) {
-    $scope.message = 'Esta es la página "Acerca de"';
-});
-
-rulesApp.controller('homeController', function($scope) {
-    $scope.message = 'Esta es la página de "Contacto", aquí podemos poner un formulario';
-});
-
-rulesApp.controller('userController', function($scope) {
-    $scope.message = 'Usuarios de la aplicación';
-    $scope.users={};
-  	$scope.getAllUsers = function($http){
-      $scope.users=[{"id":11,"firstname":"Lilia","lastname":"Espinosa"},
-                    {"id":15,"firstname":"Martha Patricia","lastname":"Espinosa"},
-                    {"id":26,"firstname":"Marta Patricia","lastname":"Espinosa"},
-                    {"id":34,"firstname":"Juan Antonio","lastname":"Perez"},
-                    {"id":35,"firstname":"José Miguel","lastname":"Mora"},
-                    {"id":36,"firstname":"Juan","lastname":"Gonzales"}];
-  	}
+  $scope.message = 'Esta es la página "Acerca de"';
 });
 
 rulesApp.controller('parserMenu', function($scope, $http) {
